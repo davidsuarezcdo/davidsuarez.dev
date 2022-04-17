199e8848ff6bde02340cdbfa724b27626fc64c40
David Suárez <contact@davidsuarez.dev>
2022-04-17 13:40:26 -0500

    solución a error de font-awesome

--- src/plugins/fontawesome.ts
+++ src/plugins/fontawesome.ts
@@ -1,11 +1,10 @@
 import Vue from "vue";
 
-import { library } from "@fortawesome/fontawesome-svg-core";
+import { IconDefinition, library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-library.add(fas);
-library.add(fab);
+library.add(<unknown>fab as IconDefinition, <unknown>fas as IconDefinition);
 
 Vue.component("font-awesome-icon", FontAwesomeIcon);
